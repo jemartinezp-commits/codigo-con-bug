b1261b3cf1472d0f21637fa843fd67970075f799
Eli <jemartinezp@alumno.uspg-edu.gt>
2026-02-11 10:18:34 -0600

    bug corregido

--- codigo_con_bug.py
+++ codigo_con_bug.py
@@ -1,12 +1,6 @@
-edad = input("Ingresa tu edad: ")
+edad = int(input("Ingresa tu edad: "))
 
-# Debug
-print("DEBUG tipo:", type(edad))
-print("DEBUG valor:", [edad])
-print("DEBUG longitud:", len(edad))
-
-# Bug aquí 👇
 if edad >= 18:
     print("Eres mayor de edad")
 else:
-    print("Eres menor de edad") 
+    print("Eres menor de edad")
